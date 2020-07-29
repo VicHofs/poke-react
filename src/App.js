a3820450460cc0e6fb09e7686f8e9b46e02ccf87
Victor Hofstetter <57238498+VicHofs@users.noreply.github.com>
2020-07-28 22:36:02 -0300

    🔍 add search bar and extra styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import api from './services/api.js'
+import api from './services/api.js';
+import { AiOutlineSearch } from 'react-icons/ai';
 import './App.css';
 
 function App() {
   return (
-    <header>
-      <h1>
-        PoKé React
-      </h1>
-    </header>
+    <div className="mainView">
+      <header>
+        <h1>
+          PoKé React
+        </h1>
+      </header>
+      <div className="headerSpacer"></div>
+      <div className="searchBar">
+        <table className="container">
+          <tr>
+            <td>
+              <input type="text" placeholder="Got a favorite Pokémon?" className="searchBox"/>
+            </td>
+            <td>
+              <AiOutlineSearch className="searchIcon"/>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
   );
 }
 
