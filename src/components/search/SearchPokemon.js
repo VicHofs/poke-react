a948086669f0fd5aa37b028e0ea1b8b16392efc7
Victor Hofstetter <57238498+VicHofs@users.noreply.github.com>
2020-07-29 11:36:10 -0300

    🔎 add pokemon search

---
+++ src/components/search/SearchPokemon.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import '../../App.css'
+
+function Search(word) {
+  let { search } = word;
+  const [data, setData] = useState([]);
+
+  const getAbility = (json) => {
+    fetch(`https://pokeapi.co/api/v2/ability/${json.id}`)
+      .then(res => res.json())
+      .then(res => {
+        setData({
+          id: json.id,
+          name: json.name,
+          image: json.sprites.front_shiny,
+          effect: res.effect_entries
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  const getPokemon = (search) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+      .then(res => res.json())
+      .then(json => {
+        setData({
+          id: json.id,
+          name: json.name,
+          image: json.sprites.front_shiny
+        });
+        getAbility(json)
+      })
+      .catch(function (error) {
+        console.log('aqui getpoke', error);
+      });
+  }
+
+  const renderData = () => {
+    let pokeData = <>
+
+    </>
+
+    if (data.effect !== undefined) {
+      return (
+      <>
+        <td><img src={data.image} alt={data.name}></img></td>
+        <td>{data.id}</td>
+        <td>{data.name}</td>
+        {data.effect.map(effect => <td>{effect.effect}</td>)}
+      </>
+    )
+    } else if (data !== undefined) {
+      return (
+      <>
+        <td><img src={data.image} alt={data.name}></img></td>
+        <td>{data.id}</td>
+        <td>{data.name}</td>
+      </>)
+    }
+    return null
+  }
+
+
+  useEffect(() => {
+    getPokemon(search)
+  }, [search]);
+
+  //console.log(data)
+
+  return (
+
+    <table className="container">
+      <tr>
+        {renderData()}
+      </tr>
+
+    </table>
+  )
+}
+
+export default Search;
