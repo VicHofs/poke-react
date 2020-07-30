0910f4fd263eb35fdbb6fba2feb17fa02c1527ef
Luann Sapucaia <luannsapucaia@gmail.com>
2020-07-30 15:20:10 -0300

    adicionado o esboço da função do dropdown

--- src/components/dropdownMenu/DropdownMenu.js
+++ src/components/dropdownMenu/DropdownMenu.js
@@ -3,25 +3,68 @@ import './DropdownMenu.css'
 
 export default function DropDownMenu(props) {
     let {dataOfPokemons} = props;
-    const [menuOptions, setMenuOptions] = useState({ results: [] });
+    let arrayOfPokemons = [props]
+    // const [menuOptions, setMenuOptions] = useState({ results: [] });
 
-    useEffect(() => {
-        const fetchResults = dataOfPokemons.map(res => ({
-            language : res.language.name,
-            version: res.version.name
-        }));
-        setMenuOptions({fetchResults});
-    }, []);
-    console.log(menuOptions, props);
+    // useEffect(() => {
+    //     const fetchResults = dataOfPokemons.map(res => ({
+    //         language : res.language.name,
+    //         version: res.version.name
+    //     }));
+    //     setMenuOptions({fetchResults});
+    // }, []);
+    // console.log(menuOptions);
+    console.log(props)
 
+if (props != null)
     return(
         <select>
-            {menuOptions.results.map(item => (
-                <option>{item.language.name} -- {item.version.name} </option>
+            {dataOfPokemons.map(item => (
+                <option>{item.data.arrayText} -- </option>
             ))}
         </select>
     )
 }
+
+// export default function DropdownMenu(obj) {
+//     let {data} = obj;
+//     const [showMenu, setShowMenu] = useState(false);
+
+//     // const isMenuVisible = () =>{console.log("hello")
+//     //     showMenu ? hideOptions() : ShowOptions();
+//     // }
+
+//     // const hideOptions = () =>{
+//     //     setShowMenu(false);
+//     //     document.getElementsByClassName("dropdown-content")[0]
+//     //         .innerHTML= ""
+//     // }
+
+//     const ShowOptions = () =>{
+//         setShowMenu(true)
+//         //console.log(data.arrayText)
+//         const map = data.arrayText.map( text => 
+//             `<button className="option">${text.language.name} -- ${text.version.name}</button>`
+                
+//             )
+//         //  document.getElementsByClassName("dropdown-content")[0]
+//         //     .innerHTML= map.toString().replace(/,/g, "")
+//         }
+
+//     return (
+//         // <div className="dropdown_menu">
+//         //     <button onClick={isMenuVisible} className="dropbtn">
+//         //         language -- version 
+//         //     </button>
+//         //     <div className="dropdown-content">
+                
+//         //     </div>
+//         // </div>
+//         <select>
+//             <option>${text.language.name} -- ${text.version.name} </option>
+//         </select>
+//     )
+// }
 
 // export default function DropdownMenu(obj) {
 //     let {data} = obj;
