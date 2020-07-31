24364db3d116d8c9c53f8c9e35ab9387b01f95a5
Victor Hofstetter <57238498+VicHofs@users.noreply.github.com>
2020-07-30 21:06:36 -0300

    add dynamic flavor defaults

--- src/components/buttons/Dropdown.js
+++ src/components/buttons/Dropdown.js
@@ -2,11 +2,19 @@ import React from "react";
 import "./Dropdown.css";
 
 export default function Dropdown(props) {
+
+    const search = (lang) => {
+        if (lang === 'en' && noDefault) {
+            noDefault = false;
+            return true;
+        }
+    }
     let flavorTexts = props.data;
+    let noDefault = true;
     console.log('dropdown got: ', flavorTexts);
     return(
         <select className="listDrop" onChange={props.onChange}>
-            {flavorTexts.map((entry) => <option className="listElement" value={`${entry.language.name}—${entry.version.name}`} key={`${entry.language.name}—${entry.version.name}`} selected={entry.language.name === 'en' && entry.version.name === 'alpha-sapphire'}>{entry.language.name} — {entry.version.name}</option>)}
+            {flavorTexts.map((entry) => <option className="listElement" value={`${entry.language.name}—${entry.version.name}`} key={`${entry.language.name}—${entry.version.name}`} selected={() => search(entry.language.name)}>{entry.language.name} — {entry.version.name}</option>)}
         </select>
     );
 }
