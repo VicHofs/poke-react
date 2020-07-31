3015cb2eba2d992d15bfec02f5bc9a7c3ad8ccd5
Victor Hofstetter <57238498+VicHofs@users.noreply.github.com>
2020-07-30 21:27:08 -0300

    add dynamic dropdown defaults

--- src/components/buttons/Dropdown.js
+++ src/components/buttons/Dropdown.js
@@ -3,10 +3,17 @@ import "./Dropdown.css";
 
 export default function Dropdown(props) {
     let flavorTexts = props.data;
+    let targetVer = 'alpha_sapphire';
+    for (let entry of flavorTexts) {
+        if (entry.language.name === 'en') {
+            targetVer = entry.version.name;
+            break;
+        }
+    }
     console.log('dropdown got: ', flavorTexts);
     return(
         <select className="listDrop" onChange={props.onChange}>
-            {flavorTexts.map((entry) => <option className="listElement" value={`${entry.language.name}—${entry.version.name}`} key={`${entry.language.name}—${entry.version.name}`} selected={entry.language.name === 'en' && entry.version.name === 'alpha-sapphire'}>{entry.language.name} — {entry.version.name}</option>)}
+            {flavorTexts.map((entry) => <option className="listElement" value={`${entry.language.name}—${entry.version.name}`} key={`${entry.language.name}—${entry.version.name}`} selected={entry.language.name === 'en' && entry.version.name === targetVer}>{entry.language.name} — {entry.version.name}</option>)}
         </select>
     );
 }
