1e0be0b78f9897316eea8becd5598528cfd0f216
Victor Hofstetter <57238498+VicHofs@users.noreply.github.com>
2020-07-31 22:33:39 -0300

    Updates

---
+++ precache-manifest.fe4534b1150f333abecd55705a166908.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55be7f40d4726b4a7d9422466ead8492",
+    "url": "/poke-react/index.html"
+  },
+  {
+    "revision": "5ddb936ee445ae2fa074",
+    "url": "/poke-react/static/css/main.12c783d3.chunk.css"
+  },
+  {
+    "revision": "1fb338c041816016a872",
+    "url": "/poke-react/static/js/2.685ae31f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poke-react/static/js/2.685ae31f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ddb936ee445ae2fa074",
+    "url": "/poke-react/static/js/main.b498d482.chunk.js"
+  },
+  {
+    "revision": "ffde6b98ac380584c93c",
+    "url": "/poke-react/static/js/runtime-main.2bf59e40.js"
+  },
+  {
+    "revision": "7e98bfcef67ba7635d02ca100e592336",
+    "url": "/poke-react/static/media/Pocket-Monk.7e98bfce.otf"
+  },
+  {
+    "revision": "b1413c1952ed902e955adf7fcc31792c",
+    "url": "/poke-react/static/media/loader.b1413c19.png"
+  },
+  {
+    "revision": "72e3f806007413aefbe3df5b352153d1",
+    "url": "/poke-react/static/media/pokemon_fire_red.72e3f806.otf"
+  }
+]);
